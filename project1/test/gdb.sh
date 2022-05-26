5f1c16455ac9bf79d7d99c57e26b37db1673cf3d
Zzxy1999 <60173759+zhuxinyu1999@users.noreply.github.com>
2022-05-26 16:48:19 +0800

    Delete gdb.sh

--- project1/test/gdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-killall -w server
-
-gdb --args ../src/server 8080 8443 log.txt lock /home/zzxy/Documents/15441/project1/static /home/zzxy/Documents/15441/project1/cgi-bin ../cert/server_priv.key ../cert/cert.crt
